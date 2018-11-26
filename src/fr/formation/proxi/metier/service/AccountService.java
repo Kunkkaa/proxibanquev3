c69801cdb85bb3462d52168ac042a2dcece46e8e
Khantain <quentinjouhault@gmail.com>
2018-11-26 15:47:27 +0100

    Correction conflit AccountService

--- src/fr/formation/proxi/metier/service/AccountService.java
+++ src/fr/formation/proxi/metier/service/AccountService.java
@@ -7,26 +7,21 @@ import fr.formation.proxi.metier.entity.Account;
 import fr.formation.proxi.persistance.AccountDao;
 
 public class AccountService {
-	
-private static final AccountService INSTANCE = new AccountService();
-private AccountDao dao = new AccountDao();
-	
+
+	private static final AccountService INSTANCE = new AccountService();
+	private AccountDao dao = new AccountDao();
 
 	public static AccountService getInstance() {
 		return AccountService.INSTANCE;
 	}
 
-<<<<<<< HEAD
-	public List<Account> getAll(Integer idClient){
-		List<Account>  accounts = this.dao.readAllAccountsForOneClient(idClient);
-		
+	public List<Account> getAll(Integer idClient) {
+		List<Account> accounts = new ArrayList<>();
+
+		accounts = this.dao.readAllAccountsForOneClient(idClient);
+
 		return accounts;
-		
+
 	}
-=======
-	
-	
-	
-	
->>>>>>> 636200fa345d3f4077ecfaf2ee6086c4051197b7
+
 }
