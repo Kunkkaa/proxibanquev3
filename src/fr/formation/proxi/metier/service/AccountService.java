d0a46e1031e6fbfbda19f09e1a2a634147fb7e16
Khantain <quentinjouhault@gmail.com>
2018-11-26 15:45:01 +0100

    Az

--- src/fr/formation/proxi/metier/service/AccountService.java
+++ src/fr/formation/proxi/metier/service/AccountService.java
@@ -1,12 +1,25 @@
 package fr.formation.proxi.metier.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import fr.formation.proxi.metier.entity.Account;
+import fr.formation.proxi.persistance.AccountDao;
+
 public class AccountService {
 	
 private static final AccountService INSTANCE = new AccountService();
+private AccountDao dao = new AccountDao();
 	
 
 	public static AccountService getInstance() {
 		return AccountService.INSTANCE;
 	}
 
+	public List<Account> getAll(Integer idClient){
+		List<Account>  accounts = this.dao.readAllAccountsForOneClient(idClient);
+		
+		return accounts;
+		
+	}
 }
