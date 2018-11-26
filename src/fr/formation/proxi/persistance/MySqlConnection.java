3cca22ff9590dee22f7c5f3b778c8454eea4cf68
Khantain <quentinjouhault@gmail.com>
2018-11-26 12:51:01 +0100

    Ajout Connection BDD

---
+++ src/fr/formation/proxi/persistance/MySqlConnection.java
@@ -0,0 +1,37 @@
+package fr.formation.proxi.persistance;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MySqlConnection {
+
+	public static final MySqlConnection INSTANCE = new MySqlConnection();
+
+	public static MySqlConnection getinstance() {
+		return MySqlConnection.INSTANCE;
+	}
+
+	private Connection conn;
+	private boolean isConnected;
+
+	public Connection getConn() {
+		return conn;
+	}
+
+	public MySqlConnection() {
+		this.isConnected = true;
+
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+
+			this.conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/proxi2?serverTimezone=Europe/Paris",
+					"root", "root");
+		} catch (SQLException | ClassNotFoundException e) {
+			this.isConnected = false;
+			e.printStackTrace();
+		}
+
+	}
+
+}
