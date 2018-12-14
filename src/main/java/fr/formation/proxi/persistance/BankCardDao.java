98737d075a72a5f8ceb5a69dacb373921007175e
Jérémy <elvynia@gmail.com>
2018-12-14 16:48:34 +0100

    Ajout du DAO pour la carte bancaire.

---
+++ src/main/java/fr/formation/proxi/persistance/BankCardDao.java
@@ -0,0 +1,38 @@
+package fr.formation.proxi.persistance;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.TypedQuery;
+
+import fr.formation.proxi.metier.entity.BankCard;
+
+public class BankCardDao extends AbstractDao<BankCard> {
+	
+	private static final BankCardDao INSTANCE = new BankCardDao();
+
+	public static BankCardDao getInstance() {
+		return BankCardDao.INSTANCE;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public BankCard read(Integer id) {
+		return this.read(id, new BankCard());
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public List<BankCard> readAll() {
+		List<BankCard> cards = new ArrayList<>();
+		TypedQuery<BankCard> query = this.em
+				.createQuery(JpqlQueries.SELECT_ALL_BANKCARD, BankCard.class);
+		cards.addAll(query.getResultList());
+		return cards;
+	}
+
+}
